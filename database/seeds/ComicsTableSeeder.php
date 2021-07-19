044f16c5da491767f49a590efa9a5b2103d35a86
DamianoTrifone <trifone.damiano@gmail.com>
2021-07-19 16:16:29 +0200

    Seeder

--- database/seeds/ComicsTableSeeder.php
+++ database/seeds/ComicsTableSeeder.php
@@ -12,6 +12,21 @@ class ComicsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $arrayComics = config('comics');
+
+        foreach ($arrayComics as $item){
+            $Comics = new Comics();
+
+            $Comics->title = $item["title"];
+            $Comics->description = $item["description"];
+            $Comics->thumb = $item["thumb"];
+            $Comics->price = $item["price"];
+            $Comics->series = $item["series"];
+            $Comics->sale_date = $item["sale_date"];
+            $Comics->type = $item["type"];
+
+            $Comics->save();
+
+        }
     }
 }
